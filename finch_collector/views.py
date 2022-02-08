3b4c344ab4ae776a67a1f8301bf261e53ca8cd69
Alos22 <aniriver689@gmail.com>
2022-02-08 15:53:21 -0500

    add template for home page

--- finch_collector/views.py
+++ finch_collector/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.views import View # view class to handle requests
+from django.views.generic.base import TemplateView # View class to handle requests
 from django.http import HttpResponse # a class to handle sending a type of response
 
 # Create your views here.
 
-class Home(View):
-
-    def get(self, request):
-        return HttpResponse("Finch Collector Home")
+class Home(TemplateView):
+    template_name = "home.html"
