07a5e98712aba1f780751f7c8b6fef0233da2d31
Alos22 <aniriver689@gmail.com>
2022-02-08 15:47:05 -0500

    changed location of views.py

---
+++ finch_collector/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views import View # view class to handle requests
+from django.http import HttpResponse # a class to handle sending a type of response
+
+# Create your views here.
+
+class Home(View):
+
+    def get(self, request):
+        return HttpResponse("Finch Collector Home")
