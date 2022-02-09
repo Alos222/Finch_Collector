176435506c074834b403e19c9b738e7aadd855d6
Alos22 <aniriver689@gmail.com>
2022-02-09 17:47:52 -0500

    added sql file to seed DB, base html update

--- db/create_finchs.sql
+++ db/create_finchs.sql
@@ -1,3 +1,20 @@
 INSERT INTO main_app_finch(name, img, bio, verified_finch, created_at)
   VALUES
-    ('House Finch','https://live.staticflickr.com/5830/23535841230_018e36d83b_b.jpg','What bird has a rosy red head, a pleasant song, is native to North America, and was once commonly kept as a cage bird? The house finch! This bird hails from the western coast of the U.S. and parts of Mexico. In the early 20th century, house finches were caught and brought to the East Coast, where pet stores sold them as Hollywood finches. Since their diet is entirely vegetarian, they were easy to maintain in a cage and would sing to their owners content.','t','2021-05-07 18:57:42.517205-07');
+    ('American Goldfinch','https://www.allaboutbirds.org/guide/assets/photo/306710541-480px.jpg','American Goldfinches are active, acrobatic finches that balance on the seedheads of thistles, dandelions, and other plants to pluck seeds. They have a bouncy flight during which they frequently make their po-ta-to-chip calls. Although males sing exuberantly during spring, pairs do not nest until mid-summer, when thistles and other weeds have gone to seed. Goldfinches do not join other songbirds mobbing predators.','t','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_finch(name, img, bio, verified_finch, created_at)
+  VALUES
+    ('Black Rosy-Finch','https://www.allaboutbirds.org/guide/assets/photo/308040821-480px.jpg','Above treeline in the mountains of the western United States resides a chunky finch seemingly oblivious to cold and snow. The Black Rosy-Finch nests in cliffs and crevices in alpine areas where few people go. This nearly black finch has a gray cap and pink highlights on the wings and belly. It descends to slightly lower elevations in the winter when snow cover is deep, foraging at the edges of melting snow, along roadsides, or at feeders.','t','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_finch(name, img, bio, verified_finch, created_at)
+  VALUES
+    ('Blue Grosbeak','https://www.allaboutbirds.org/guide/assets/photo/67447361-480px.jpg','A large, vibrantly blue bunting with an enormous silver bill and chestnut wingbars, the male Blue Grosbeak sings a rich, warbling song from trees and roadside wires. He and his cinnamon-colored mate often raise two broods of nestlings in a single breeding season. A bird of shrubby habitats, these richly colored birds can be hard to spot unless you hear them singing first. They are widespread but not abundant across the southern U.S., and are expanding their range.','t','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_finch(name, img, bio, verified_finch, created_at)
+  VALUES
+    ('Brown-capped Rosy-Finch','https://www.allaboutbirds.org/guide/assets/photo/67447361-480px.jpg','With its rosy pink belly and brown upperparts, the Brown-capped Rosy-Finch looks like raspberry ice cream smothered in chocolate. This rosy-finch breeds almost entirely in Colorado, in high alpine areas near remote glaciers and snowy meadows where they feed on seeds and insects along the edge of melting snow. In winter, they descend in flocks into forested habitats at lower elevations. They often show up in large numbers at feeders in mountain communities.','t','2021-05-07 18:57:42.517205-07');
+
+INSERT INTO main_app_finch(name, img, bio, verified_finch, created_at)
+  VALUES
+    ('Cassia Crossbill','https://www.allaboutbirds.org/guide/assets/photo/306391451-480px.jpg','The Cassia Crossbill, a finch with a crisscrossed bill, is closely related to the widespread Red Crossbill and was recognized as a full species in 2017. Unlike the nomadic Red Crossbill, the Cassia stays put year-round in a single county in Idaho, feeding on lodgepole pine cones that the Red Crossbill cant open. Groups of the red and yellow finches dangle from cones, quietly munching on seeds, until they erupt into flight with a kip. The species small population and geographic isolation makes it vulnerable to extinction.','t','2021-05-07 18:57:42.517205-07');
+
